be42791071666c83c529448d97a254b37b863569
will-twd <hello@willthewebdesigner.com>
2025-03-18 13:28:51 +0000

    passes basic tests but needs to return ERROR if number is negative

--- 02_repeatString/repeatString.spec.js
+++ 02_repeatString/repeatString.spec.js
@@ -4,23 +4,23 @@ describe('repeatString', () => {
   test('repeats the string', () => {
     expect(repeatString('hey', 3)).toEqual('heyheyhey');
   });
-  test.skip('repeats the string many times', () => {
+  test('repeats the string many times', () => {
     expect(repeatString('hello', 10)).toEqual('hellohellohellohellohellohellohellohellohellohello');
   });
-  test.skip('repeats the string 1 times', () => {
+  test('repeats the string 1 times', () => {
     expect(repeatString('hi', 1)).toEqual('hi');
   });
-  test.skip('repeats the string 0 times', () => {
+  test('repeats the string 0 times', () => {
     expect(repeatString('bye', 0)).toEqual('');
   });
-  test.skip('does not use the built-in String repeat method', () => {
+  test('does not use the built-in String repeat method', () => {
     /* Even though there is a built-in String repeat method,
       in this exercise specifically, we want you to practise using loops */
     jest.spyOn(String.prototype, 'repeat').mockName('Built-in String repeat method');
     repeatString("don't use the built-in repeat method!", 1);
     expect(String.prototype.repeat).not.toHaveBeenCalled();
   });
-  test.skip('returns ERROR with negative numbers', () => {
+  test('returns ERROR with negative numbers', () => {
     expect(repeatString('goodbye', -1)).toEqual('ERROR');
   });
   test.skip('repeats the string a random amount of times', function () {
