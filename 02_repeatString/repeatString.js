c4573a124e2bc644033c2b694c1f2d55f30c9783
will-twd <hello@willthewebdesigner.com>
2025-03-18 13:20:51 +0000

    passes first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeat = "";
+        while (num > 0) {
+            repeat += string;
+            num--;
+        }
+    return repeat;
 };
 
 // Do not edit below this line
